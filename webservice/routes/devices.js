7c65ccd7a6cde9640eaf4446a22e30e0476532f0
Prashant Yadav <pyadav@PYadav-MBR01.local>
2016-04-17 23:46:27 -0700

    Cassandra REST service GET

--- webservice/routes/devices.js
+++ webservice/routes/devices.js
@@ -5,6 +5,52 @@ var fc = require('../utility/functions');
 var cassandra =  require('cassandra-driver');
 
 
+exports.getDeviceData = function(req, res) {
+  console.log("************************channel******************"+req.params.channel);
+  cquery = "select * from refrigerator where channel='"+req.params.channel+"'";
+
+  console.log("*****************************cquery********************************"+cquery);
+  client_cas.execute(cquery, function (err, result) {
+    if (typeof result === 'undefined')
+      console.log("No Data");
+    else {
+      if (result.rows.length === 0) {
+        console.log("No Data");
+        res.send("No Data for this channel");
+      }
+      else {
+        console.log("********************** RESULT*****************"+result);
+        res.send(result);
+        // AlljsonServices='[';
+
+        // for (var i = 0; i < result.rows.length; i++) {
+
+        //   service = result.rows[i];
+
+        //   jsonService = '{ '
+        //   + '"service_id" : "'+ service.service_id + '", '
+        //   + '"title" : "'+ service.title + '", '
+        //   + '"description" : "'+ service.description + '", '
+        //   + '"date_creation" : "'+ service.date_creation + '", '
+        //   + '"duration" : "'+ service.duration + '", '
+        //   + '"type" : "'+ service.type + '", '
+        //   + '"usage_date" : "'+ service.usage_date + '", '
+        //   + '"username" : "'+ service.username + '"},';
+
+        //   AlljsonServices=AlljsonServices+jsonService;
+        // }
+
+        // AlljsonServices=AlljsonServices.slice(0, - 1)+']';
+
+        // return reply(AlljsonServices);
+      }
+    }
+
+  });
+}
+
+
+
 exports.addDeviceData = function(req, res) {
     var payload = req.body;
     var device_id = payload.device_id;
